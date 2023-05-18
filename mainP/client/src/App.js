a00b57e1408751e7562d66cfbbf146d65e76ca61
Arif <0222learning@gmail.com>
2023-05-19 00:02:38 +0530

    username componet completed using formik and react-hot-toast

--- mainP/client/src/App.js
+++ mainP/client/src/App.js
@@ -2,7 +2,14 @@
 import React from "react";
 import {createBrowserRouter , RouterProvider} from 'react-router-dom'
 
+// importing all the 
 import Home from "./components/home/Home";
+import Password from "./components/Authentication/Password";
+import Recovery from "./components/Authentication/Recovery";
+import Register from "./components/Authentication/Register";
+import Reset from "./components/Authentication/Reset";
+import Username from "./components/Authentication/Username";
+
 
 // root routes
 const router = createBrowserRouter([
@@ -17,7 +24,23 @@ const router = createBrowserRouter([
   {
     path : "/login",
     element : <div>login route</div>
-  }
+  },
+  {
+    path : "/Password",
+    element : <div>Password route</div>
+  },
+  {
+    path : "/Recovery",
+    element : <div>recovery route</div>
+  },
+  {
+    path : "/Reset",
+    element : <div>reset route</div>
+  },
+  {
+    path : "/Username",
+    element : <div><Username></Username></div>
+  },
   
 ])
 function App() {
