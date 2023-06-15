ffe0d1e32caa3e1317c35624f41dc527447378d1
Arif <0222learning@gmail.com>
2023-06-15 19:22:59 +0530

    removed manual auth components

--- mainP/client/src/App.js
+++ mainP/client/src/App.js
@@ -4,11 +4,9 @@ import {createBrowserRouter , RouterProvider} from 'react-router-dom'
 
 // importing all the 
 import Home from "./components/home/Home";
-import Password from "./components/Authentication/Password";
-import Recovery from "./components/Authentication/Recovery";
-import Register from "./components/Authentication/Register";
-import Reset from "./components/Authentication/Reset";
-import Username from "./components/Authentication/Username";
+
+
+
 
 
 // root routes
@@ -17,30 +15,8 @@ const router = createBrowserRouter([
     path : "/",
     element : <div><Home></Home></div>
   },
-  {
-    path : "/register",
-    element : <div>register route</div>
-  },
-  {
-    path : "/login",
-    element : <div>login route</div>
-  },
-  {
-    path : "/Password",
-    element : <div>Password route</div>
-  },
-  {
-    path : "/Recovery",
-    element : <div>recovery route</div>
-  },
-  {
-    path : "/Reset",
-    element : <div>reset route</div>
-  },
-  {
-    path : "/Username",
-    element : <div><Username></Username></div>
-  },
+ 
+  
   
 ])
 function App() {
--- mainP/client/src/components/home/Navbar.jsx
+++ mainP/client/src/components/home/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
+import '../Styles/Navbar.css'
 
 const Navbar = () => {
   const { loginWithRedirect , isAuthenticated , logout , user } = useAuth0();
