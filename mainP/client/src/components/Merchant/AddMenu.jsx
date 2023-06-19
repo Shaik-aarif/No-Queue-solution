87291969049d249b8784f9753353bee30d5690e6
Arif <0222learning@gmail.com>
2023-06-19 08:39:35 +0530

    completed partial work on addmenu page

--- mainP/client/src/components/Merchant/AddMenu.jsx
+++ mainP/client/src/components/Merchant/AddMenu.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import "../Styles/Merchant_styles/AddMenu.css"
 import Navbar from '../home/Navbar'
-import {MdFastfood} from 'react-icons/md'
+import Loader from './Loader'
+import {MdFastfood , MdCloudUpload , MdDelete , MdAccessTimeFilled  , MdCurrencyRupee} from 'react-icons/md'
 import {motion} from "framer-motion"
 const AddMenu = () => {
 
@@ -15,6 +16,9 @@ const AddMenu = () => {
     const [imageAsset , setImageAsset] = useState(null);
     
 
+    const uploadImage  = () =>{}
+    const deleteImage  = () =>{}
+
 
 
   return (
@@ -27,13 +31,14 @@ const AddMenu = () => {
       <div className="item-border w-[90%] md:w-[75%]  rounded-lg py-3 flex items-center justify-center flex-col ">
         {
             fields && (
-                <p className={`w-[95%] text-center rounded-lg p-2  font-semibold ${alertStatus === 'danger' ? 
+                <p className={`w-[95%] text-center rounded-lg p-2   ${alertStatus === 'danger' ? 
                 'bg-red-400 text-red-800' : 'bg-green-400 text-green-800'
-                }`}>{msg}</p>
+                }`}>{msg}Error message</p>
             )
         }
+        
        <div className="item-name w-[90%] py-2 border-b border-gray-300 flex  items-center justify-start ml-2  gap-2">
-          <MdFastfood  className="text-xl text-grey-700"/>
+          <MdFastfood  className="text-xl icons text-grey-400"/>
           <input
             type = "text"
             required
@@ -48,9 +53,65 @@ const AddMenu = () => {
         {/* imgae laoding area */}
         <div className="group flex items-center justify-center flex-col border-2 border-dotted  my-5
         border-red-300 w-[90%] h-225 md:h-390 cursor-pointer rounded-lg">
-
+              {isLoading ? <Loader/> : <>
+                {
+                  !imageAsset ? (<>
+                    <label className="w-full h-full flex items-center flex-col justify-center cursor-pointer">
+                        <div className="w-full h-full flex items-center flex-col justify-center gap-2">
+                            <MdCloudUpload className=" icons text-3xl"></MdCloudUpload>
+                            <p className="text-grey-300  md:text-2xl icons  hover:text-grey-700">Click here to upload image</p>
+                        </div>
+                        <input type="file"
+                              name = "uploadimage"
+                              accept="image/*"
+                              onChange={uploadImage}
+                              className="w-0 h-0"
+                         />
+                    </label>
+                  </>) :  (<>
+                    <div className="relative h-full">
+                    <img src = {imageAsset} alt = "uploadedimage" className="w-full h-full object-cover" />
+                    <button
+                      type="button"
+                      className="absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none
+                      hover:shadow-md duration-500 transition-all ease-in-out"
+                      onClick={deleteImage}
+                    />
+                    <MdDelete className ="text-white" 
+                    />
+                    </div>
+                  </> 
+                )}
+              </>}
         </div>
-
+            
+            {/* price section */}
+            <div className="item-name w-[90%] py-2 border-b border-gray-300 flex  items-center justify-start ml-2  gap-2">
+          <MdCurrencyRupee  className="text-xl icons text-grey-400"/>
+          <input
+            type = "text"
+            required
+            value = {price}
+            onChange={(e) => setPrice(e.target.value)}
+            placeholder="Enter the item price...."
+            className="w-[90%] h-full text-lg bg-transparent font-semibold outline-none border-none placeholder:text-grey-300 
+            text-grey-800"
+          />
+       </div>
+       {/* time */}
+       <div className="item-name w-[90%] py-2 border-b border-gray-300 flex  items-center justify-start ml-2  gap-2">
+          <MdAccessTimeFilled  className="text-xl icons text-grey-400"/>
+          <input
+            type = "text"
+            required
+            value = {timeTaken}
+            onChange={(e) => setTimeTaken(e.target.value)}
+            placeholder="Enter average time to prepare item...."
+            className="w-[90%] h-full text-lg bg-transparent font-semibold outline-none border-none placeholder:text-grey-300 
+            text-grey-800"
+          />
+       </div>
+       
        </div>
 
     </div>
